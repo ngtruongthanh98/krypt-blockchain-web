1c14bb3d0435325c6518e899081ca3cdc6e805d1
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2022-05-06 22:51:19 +0700

    Ethereum card.

--- client/src/components/Welcome/index.jsx
+++ client/src/components/Welcome/index.jsx
@@ -54,6 +54,11 @@ const Welcome = () => {
                 </div>
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
+
+              <div>
+                <p className="text-white font-light text-sm">Address</p>
+                <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
+              </div>
             </div>
           </div>
         </div>
